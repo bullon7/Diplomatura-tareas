bee68c2b52e7918e40d3cb990832f9a0cbda8de4
Martina Bullon <martubullon@gmail.com>
2023-08-03 16:25:16 -0300

    Tarea M6U2

---
+++ proyectoTattoo/back/models/promocionesModel.js
@@ -0,0 +1,43 @@
+var pool = require('./bd');
+
+async function getPromociones() {
+    var query = 'select * from promociones';
+    var rows = await pool.query(query);
+    return rows;
+}
+
+async function insertPromocion(obj) {
+    try {
+        var query = 'insert into promociones set ?';
+        var rows = await pool.query(query,[obj])
+        return rows;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+}
+
+async function deletePromocionById(id) {
+    var query = 'delete from promociones where id = ?';
+    var rows = await pool.query(query, [id]);
+    return rows;
+}
+
+async function getPromocionesById(id) {
+    var query = 'select * from promociones where id = ?';
+    var rows = await pool.query(query, [id]);
+    return rows[0];
+}
+
+async function modificarPromocionesById(obj, id) {
+    try {
+        var query = 'update promociones set ? where id= ?';
+        var rows = await pool.query(query, [obj, id]);
+        return rows;
+    } catch (error) {
+        throw error;
+    }
+}
+
+
+module.exports = {getPromociones, insertPromocion, deletePromocionById, getPromocionesById, modificarPromocionesById}
--- proyectoTattoo/back/routes/admin/login.js
+++ proyectoTattoo/back/routes/admin/login.js
@@ -28,7 +28,7 @@ router.post('/', async (req, res, next) => {
       req.session.id_usuario = data.id;
       req.session.nombre = data.usuario;
 
-      res.redirect('/admin/novedades');
+      res.redirect('/admin/promociones');
     } else {
       res.render('admin/login', {
         layout: 'admin/layout',
